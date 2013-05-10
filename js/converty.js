29b091c43c3a91dc1e5445eb6e6c0fa2e2a458ba
Hector Virgen <hvirgen@attinteractive.com>
2013-05-10 16:10:06 -0700

    Initial commit

---
+++ js/converty.js
@@ -0,0 +1,112 @@
+(function(){
+  "use strict";
+
+  var converty = angular.module('converty', []);
+
+  converty.controller('MainController', function($scope) {
+    $scope.activeConverters = [];
+    $scope.input = '';
+    $scope.output = '';
+
+    var buildOutput = function() {
+      var value = $scope.input;
+      
+      angular.forEach($scope.activeConverters, function(converter) {
+        try {
+          var converted = converter.converter(value);
+          converter.error = false;
+          value = converted;
+        } catch(e) {
+          converter.error = true;
+        }
+      });
+      
+      $scope.output = value;
+    };
+
+    $scope.availableConverters = [
+      {
+        name: 'URI Encode',
+        converter: function(value) {
+          return encodeURIComponent(value);
+        }
+      },
+      {
+        name: 'URI Decode',
+        converter: function(value) {
+          return decodeURIComponent(value);
+        }
+      },
+      {
+        name: 'HTML Encode',
+        converter: function(value) {
+          return angular.element('<div></div>').text(value).html();
+        }
+      },
+      {
+        name: 'HTML Decode',
+        converter: function(value) {
+          return angular.element('<div></div>').html(value).text();
+        }
+      },
+      {
+        name: 'Add Slashes',
+        converter: function(value) {
+          return value.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+        }
+      },
+      {
+        name: 'Strip Slashes',
+        converter: function(value) {
+          return value.replace(/\\(.?)/g, function(s, n1) {
+            switch (n1) {
+              case '\\':
+                return '\\';
+
+              case '0':
+                return '\u0000';
+
+              case '':
+                return '';
+
+              default:
+                  return n1;
+            }
+          });
+        }
+      },
+      {
+        name: 'JSON Pretty Print',
+        converter: function(value) {
+          var obj = JSON.parse(value);
+          return JSON.stringify(obj, undefined, 2);
+        }
+      }
+    ];
+
+    $scope.toggleConverter = function(converter) {
+      var index = $scope.activeConverters.indexOf(converter);
+
+      if (-1 === index) {
+        $scope.activeConverters.push(converter);
+      } else {
+        $scope.activeConverters.splice(index, 1);
+      }
+
+      buildOutput();
+    };
+
+    $scope.disableAllConverters = function() {
+      $scope.activeConverters = [];
+      buildOutput();
+    };
+
+    $scope.isConverterActive = function(converter) {
+      return -1 !== $scope.activeConverters.indexOf(converter);
+    };
+
+    $scope.$watch('input', function() {
+      buildOutput();
+    });
+  });
+}());
